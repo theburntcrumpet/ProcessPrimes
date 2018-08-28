ff52da1856930dd9d5d1a54ed0af9127d00ce1cf
theburntcrumpet <msksansom@gmail.com>
2018-08-28 18:07:53 +0100

    Batch now checks for existing repo

--- ProcessPrimes.bat
+++ ProcessPrimes.bat
@@ -1,12 +1,26 @@
 @ECHO OFF
-echo "Installing Chocolatey"
+del C:\ProcessPrimes /Q
+echo Installing Chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-echo "Installing Python and Git"
+echo Installing Python and Git
 cmd /C "choco install git python notepadplusplus -y"
 
-echo "Cloning repository"
-cmd /C "cd C:\ && git clone https://github.com/theburntcrumpet/ProcessPrimes.git"
+IF NOT EXIST C:\ProcessPrimes GOTO CLONE
+GOTO PULL
 
-echo "Running the benchmark"
+:benchmark
+echo Running the benchmark
 cmd /C "cd C:\ProcessPrimes && python ProcessPrimes.py"
+cmd /C "START notepad++ C:\ProcessPrimes\ProcessPrimes.log"
+exit
+
+:CLONE
+echo Cloning repository
+cmd /C "cd C:\ && git clone https://github.com/theburntcrumpet/ProcessPrimes.git"
+GOTO benchmark
+
+:PULL
+echo Updating repository
+cmd /C "cd C:\ProcessPrimes && git pull"
+GOTO benchmark
